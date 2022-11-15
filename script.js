d76ed918b86fb6a1ded9a0ae45ed902bdbb0bb45
Andrew Carbert <ac.believer@gmail.com>
2022-11-14 23:40:47 -0600

    update

--- script.js
+++ script.js
@@ -1,39 +1,63 @@
-private void tbInput_TextChanged(object sender, TextChangedEventArgs e)
-{
-	string[] input = tbInput.Text.Split('\n', '\r', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-	lbTimestamps.Items.Clear();
-	string[] lastLine = null;
-	foreach(string line in input)
-	{
-		string[] lineSplit = line.Split(' ', 2);
-		if (TimeSpan.TryParse(line.Split()[0], out TimeSpan timespan) && lineSplit.Length == 2) {
-			if (lastLine != null) {
-				int itemIndex = lbTimestamps.Items.Add(timespan - TimeSpan.Parse(lastLine[0]) + " " + lastLine[1]);
-				lbTimestamps.SelectedItems.Add(lbTimestamps.Items[itemIndex]);
+Number.prototype.toHHMMSS = function () {
+	var sec_num = parseInt(this, 10)
+	var hours = Math.floor(sec_num / 3600)
+	var minutes = Math.floor(sec_num / 60) % 60
+	var seconds = sec_num % 60
+
+	return [hours, minutes, seconds]
+		.map(v => v < 10 ? "0" + v : v)
+		.filter((v, i) => v !== "00" || i > 0)
+		.join(":")
+}
+
+$(window).on("load", function () {
+	$("#input").on("change keyup paste", input_TextChanged);
+	$("#select").on("change", selection_Changed);
+	$("#select").on("click", "option",
+		function (e) {
+			e.preventDefault();
+			this.parentElement.focus();
+			this.selected = !this.selected;
+			$("#select").trigger("change");
+			return true;
+		}
+	);
+});
+
+function input_TextChanged(e) {
+	var input = $(this).val().split(/\r?\n/);
+	var lastLine = null;
+	var $select = $("#select");
+	$select.empty();
+	for (var line of input) {
+		var lineSplit = line.split(' ', 2);
+
+		if (lineSplit.length == 2) {
+			var timestamp = 0;
+			timestamp = lineSplit[0].split(':').reduce((acc, time) => (60 * acc) + +time);
+
+			if (lastLine != null && timestamp - lastLine[0] > 0) {
+				var duration = (timestamp - lastLine[0]).toHHMMSS();
+				$select.append($("<option/>", { text: duration + " " + lastLine[1], selected: true }));
 			}
-			lastLine = lineSplit;
+			lastLine = [timestamp, lineSplit[1]];
 		}
 	}
 
 	if (lastLine != null) {
-		int itemIndex = lbTimestamps.Items.Add("XX:XX:XX " + lastLine[1]);
-		lbTimestamps.SelectedItems.Add(lbTimestamps.Items[itemIndex]);
+		$select.append($("<label/>").val("XX:XX:XX " + lastLine[1]));
 	}
+	$("#select").trigger("change");
 }
 
-    private void lbTimestamps_SelectionChanged(object sender, SelectionChangedEventArgs e)
-{
-	tbOutput.Clear();
-	List < string > output = new List < string > ();
-	TimeSpan totalTimespan = new TimeSpan();
-	foreach(string item in (sender as ListBox).Items)
-	{
-		if ((sender as ListBox).SelectedItems.Contains(item)) {
-			output.Add(totalTimespan.ToString(@"hh\:mm\:ss") + " " + item.Split(" ", 2)[1]);
-			if (TimeSpan.TryParse(item.Split()[0], out TimeSpan timespan)) {
-				totalTimespan += timespan;
-			}
+function selection_Changed() {
+	var output = [];
+	var totalTimespan = 0;
+	for (var item of this) {
+		if (item.selected) {
+			output.push(totalTimespan.toHHMMSS() + " " + item.value.split(" ", 2)[1]);
+			totalTimespan += item.value.split(" ", 1)[0].split(':').reduce((acc, time) => (60 * acc) + +time);
 		}
 	}
-	tbOutput.Text = String.Join('\n', output);
+	$("#output").val(output.join('\n'));
 }
